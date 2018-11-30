db822d3c89fa20c9ded6b9a4e2114b32d5c4e055
Nikolay Brel <nikolay.brel@gmail.com>
2018-11-30 17:14:35 +0300

    Initial Commit

---
+++ Newsfeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newsfeed
+//
+//  Created by Nikolay Brel on 30/11/2018.
+//  Copyright © 2018 Nikolay Brel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
