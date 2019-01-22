f1313957b01ac56ca151ec271609d976056884fd
Nikolay Brel <nikolay.brel@gmail.com>
2019-01-22 21:36:18 +0300

    code review fixes

--- Newsfeed/Networking/ApiConstants.swift
+++ Newsfeed/Networking/ApiConstants.swift
@@ -6,7 +6,7 @@
 import Foundation
 
 enum ApiConstants {
-    static let ArticleURL: String = "https://example.com/v1/"
+    static let ArticleURL: String = "https://newsapi.org/v2/top-headlines"
     static let ArticleURLParameters: [String: String] = [
         "apiKey": "fea974e1e6e14d9aae41a70962fefe59",
         "country": "us",
